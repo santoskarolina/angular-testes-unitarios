ef18c70d373a3a7626b77701693de2075b1daaac
santoskarolina <anna15soliveira@gmail.com>
2022-12-03 13:57:45 -0300

    config like module test

--- src/app/components/likeComponent/like.component.spec.ts
+++ src/app/components/likeComponent/like.component.spec.ts
@@ -1,6 +1,19 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { UniqueIdService } from "src/app/services/unique-id/unique-id.service";
 import { LikeComponent } from "./like.component";
 
 describe(LikeComponent.name, () => {
-    let component: LikeComponent;
-    beforeEach(() => component = new LikeComponent())
+    let fixture: ComponentFixture<LikeComponent>;
+    beforeEach(async() => {
+        await TestBed.configureTestingModule({
+            declarations: [LikeComponent],
+            providers: [UniqueIdService]
+        }).compileComponents();
+        fixture = TestBed.createComponent(LikeComponent)
+    })
+
+    it('should create component', () => {
+         const instance = fixture.componentInstance;
+         expect(instance).toBeTruthy()
+    })
 })
