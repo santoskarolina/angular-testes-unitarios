c44c9f41303359fdb794d369898dc6797200fca6
santoskarolina <anna15soliveira@gmail.com>
2022-12-03 14:27:02 -0300

    like component tests

--- src/app/components/likeComponent/like.component.spec.ts
+++ src/app/components/likeComponent/like.component.spec.ts
@@ -1,19 +1,37 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { UniqueIdService } from "src/app/services/unique-id/unique-id.service";
 import { LikeComponent } from "./like.component";
+import { LikeModule } from "./like.module";
 
 describe(LikeComponent.name, () => {
-    let fixture: ComponentFixture<LikeComponent>;
+    let fixture: ComponentFixture<LikeComponent>
+    let component: LikeComponent
+    let service: UniqueIdService
+
     beforeEach(async() => {
         await TestBed.configureTestingModule({
-            declarations: [LikeComponent],
-            providers: [UniqueIdService]
+           imports: [LikeModule]
         }).compileComponents();
-        fixture = TestBed.createComponent(LikeComponent)
     })
 
-    it('should create component', () => {
-         const instance = fixture.componentInstance;
-         expect(instance).toBeTruthy()
+    beforeEach(() => {
+      fixture = TestBed.createComponent(LikeComponent)
+      component = fixture.componentInstance;
+      fixture.detectChanges()
+      service = new UniqueIdService();
+    })
+
+    it(`Shoud create component ${LikeComponent.name}`, () => {
+         expect(component).toBeTruthy()
+    })
+
+    it('Shoud auto generete id when id input property is missing', () => {
+        expect(component.id).toBeTruthy()
+    })
+
+    it('Shoud not generete id when id input property is present', () => {
+        const componentID = service.generateUniqueIdWithPrefix(LikeComponent.name)
+        component.id = componentID
+        expect(component.id).toBe(componentID)
     })
 })
