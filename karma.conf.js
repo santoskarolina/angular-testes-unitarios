dc09f9c2221f3c01e5b51a8ea10701656b20eacd
santoskarolina <anna15soliveira@gmail.com>
2022-12-10 11:51:29 -0300

    custom Launchers

--- karma.conf.js
+++ karma.conf.js
@@ -40,6 +40,12 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    customLaunchers: {
+      FirefoxNoHeadless: {
+        base: 'Firefox',
+        flags: ['-headless']
+      }
+    }
   });
 };
