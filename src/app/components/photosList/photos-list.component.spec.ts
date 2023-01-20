f81e83d36faccdd51a739ac5b727d4a857b42488
anakarolina.oliveira <anakarolina.oliveira@perinity.com>
2023-01-19 22:15:49 -0300

    test: display loader

--- src/app/components/photosList/photos-list.component.spec.ts
+++ src/app/components/photosList/photos-list.component.spec.ts
@@ -3,6 +3,8 @@ import { PhotosBoardsComponent } from './../photosBoard/photos-board.component';
 import { PhotosListModule } from './photos-list.module';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PhotosListComponent } from './photos-list.component';
+import { photosArray } from '../utils/fill-photos';
+import { of } from 'rxjs';
 
 describe(PhotosListComponent.name, () => {
     let fixture: ComponentFixture<PhotosListComponent>;
@@ -26,8 +28,31 @@ describe(PhotosListComponent.name, () => {
         expect(component).toBeTruthy();
     })
 
-    it(`(DOM) Should display ${PhotosBoardsComponent.name} when data arrives`, () => {
+    it(`(DOM) Should display #loader when data arrives`, () => {
+        const photos = photosArray();
 
+        spyOn(service, 'getPhotos').and.returnValue(of(photos));
+        component.isLoading = false;
+        fixture.detectChanges();
+
+        const appPhotosBoard = fixture.nativeElement.querySelector('app-photos-board');
+        const loader = fixture.nativeElement.querySelector('.loader');
+
+        expect(appPhotosBoard).withContext('Should display appPhotosBoard').not.toBeNull();
+        expect(loader).withContext('Should not display loader').toBeNull();
     })
 
+
+    it(`(DOM) Should display #${PhotosBoardsComponent.name} when data arrives`, () => {
+        const photos = photosArray();
+
+        spyOn(service, 'getPhotos').and.returnValue(of(photos));
+        fixture.detectChanges();
+
+        const appPhotosBoard = fixture.nativeElement.querySelector('app-photos-board');
+        const loader = fixture.nativeElement.querySelector('.loader');
+
+        expect(appPhotosBoard).withContext('Should not display appPhotosBoard').toBeNull();
+        expect(loader).withContext('Should display loader').not.toBeNull();
+    })
 })
